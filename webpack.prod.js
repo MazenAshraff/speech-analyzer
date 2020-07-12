5dc52a1d7622b6f07454f6bb09e8f56b84158d77
Mazen <mazen.ashraf.12@gmail.com>
2020-07-12 14:47:47 +0200

    workbox plugin added

--- webpack.prod.js
+++ webpack.prod.js
@@ -38,6 +38,6 @@ module.exports = {
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
         }),
-        // new WorkboxPlugin.GenerateSW()
+        new WorkboxPlugin.GenerateSW()
     ]
 }
